60cdc78d1d7e02f1fa4de33cb7416ff78202f10b
jydnbach <jaydenkim95@gmail.com>
2024-06-25 16:51:04 +0900

    replaced img with Image

--- components/main-header.js
+++ components/main-header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import classes from './main-header.module.css';
 
@@ -8,8 +9,8 @@ export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link href="/" className={classes.logo}>
-        <img src={logoImg.src} alt="A plate with food on it" />
-        Next Level Food
+        <Image src={logoImg} alt="A plate with food on it" priority />
+        Next JS Food
       </Link>
 
       <nav className={classes.nav}>
